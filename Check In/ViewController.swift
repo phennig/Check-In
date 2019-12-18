ce48e7117796b6de5d38c39aa6e73395b8214dc1
phennig <paul.hennig@d214.org>
2019-12-18 10:52:17 -0600

    PickerView and Locations Array

--- Check In/ViewController.swift
+++ Check In/ViewController.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var locationPickerView: UIPickerView!
+    
+    var checkInLocations = ["Knights' Learning Center", "Commons", "Math Science Resource Room", "Lunch Room","Other"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
 
 
 }
