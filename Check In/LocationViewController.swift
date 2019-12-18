3df1000afaead11ff60315b8c15bbb820915e1b6
sdanderson2017 <sanderson2017@stu.d214.org>
2019-12-18 10:36:37 -0600

    location view controller

---
+++ Check In/LocationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationViewController.swift
+//  Check In
+//
+//  Created by period2 on 12/18/19.
+//  Copyright © 2019 Timothy P. Hennig. All rights reserved.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
