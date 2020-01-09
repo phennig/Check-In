9e1ec06153f6eeec86c897f69c6255ad4dc228ff
Arslan Khan <akhan2227@stu.d214.org>
2020-01-09 09:37:18 -0600

    Was working on textile animation

--- Check In/ViewController.swift
+++ Check In/ViewController.swift
@@ -12,27 +12,36 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     @IBOutlet weak var locationPickerView: UIPickerView!
     
-    var checkInLocations = ["Knights' Learning Center", "Commons", "Math Science Resource Room", "Lunch Room","Other"]
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-       return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return checkInLocations.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        let titleRow = checkInLocations[row]
-        return titleRow
+     var checkInLocations = ["Knights' Learning Center", "Commons", "Math Science Resource Room", "Lunch Room","Other"]
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+            
+    //        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: ViewController.keyboard, object: <#T##Any?#>)
+       }
+        //Work in process for text field - Arslan
+       
         
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+           return 1
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return checkInLocations.count
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            let titleRow = checkInLocations[row]
+            return titleRow
+            
+        }
+        
+        @objc func keyboardWillChange(notification: Notification)
+        {
+            print("JELP \(notification.name.rawValue)")
+        }
+        //work in progress - Arslan
+
     }
-    
 
-
-}
-
+        // Dan : - )
