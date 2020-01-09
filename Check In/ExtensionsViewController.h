3370f2d397d7dfc7de6fce5931a149d923066b8b
Arslan Khan <akhan2227@stu.d214.org>
2020-01-09 09:58:00 -0600

    Working On Log in Page

---
+++ Check In/ExtensionsViewController.h
@@ -0,0 +1,17 @@
+//
+//  ExtensionsViewController.h
+//  Check In
+//
+//  Created by period5 on 1/9/20.
+//  Copyright © 2020 Timothy P. Hennig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ExtensionsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Check In/ViewController.swift
+++ Check In/ViewController.swift
@@ -8,11 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController{
     
-    @IBOutlet weak var locationPickerView: UIPickerView!
-    
-     var checkInLocations = ["Knights' Learning Center", "Commons", "Math Science Resource Room", "Lunch Room","Other"]
+
         override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view.
@@ -22,19 +20,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         //Work in process for text field - Arslan
        
         
-        func numberOfComponents(in pickerView: UIPickerView) -> Int {
-           return 1
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-            return checkInLocations.count
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-            let titleRow = checkInLocations[row]
-            return titleRow
-            
-        }
+       
         
         @objc func keyboardWillChange(notification: Notification)
         {
