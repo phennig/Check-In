5f1b56e27456a951518c2cbdb955565b447f1f2b
Arslan Khan <akhan2227@stu.d214.org>
2020-01-13 09:52:01 -0600

    Design of buttons

---
+++ Check In/ButtonsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ButtonsViewController.swift
+//  Check In
+//
+//  Created by period5 on 1/13/20.
+//  Copyright © 2020 Timothy P. Hennig. All rights reserved.
+//
+
+import UIKit
+
+class ButtonsViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "ButtonsPage.png")!)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
