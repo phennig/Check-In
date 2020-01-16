1b3a7e0d703683c0fae13d3b17e6687eb8a263e8
Fatih Balsoy <fatih.balsoy@gmail.com>
2020-01-15 19:55:19 -0600

    - New Frameworks: Firebase/Core and MaterialComponents

--- Podfile
+++ Podfile
@@ -4,9 +4,13 @@
 target 'Check In' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
-pod 'Firebase/Analytics'
-pod 'Firebase/Firestore'
+  pod 'Firebase/Core'       # https://github.com/firebase/firebase-ios-sdk
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Firestore'
+  pod 'MaterialComponents'  # https://github.com/material-components/material-components-ios
+  
   # Dan was here
+  # Moe was here
 
   # Pods for Check In
 
--- Pods/Target Support Files/Pods-Check In/Pods-Check In-frameworks.sh
+++ Pods/Target Support Files/Pods-Check In/Pods-Check In-frameworks.sh
@@ -163,6 +163,11 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BoringSSL-GRPC/openssl_grpc.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MDFInternationalization/MDFInternationalization.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MDFTextAccessibility/MDFTextAccessibility.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MaterialComponents/MaterialComponents.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MotionAnimator/MotionAnimator.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MotionInterchange/MotionInterchange.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/abseil/absl.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/gRPC-C++/grpcpp.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/gRPC-Core/grpc.framework"
@@ -172,6 +177,11 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BoringSSL-GRPC/openssl_grpc.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MDFInternationalization/MDFInternationalization.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MDFTextAccessibility/MDFTextAccessibility.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MaterialComponents/MaterialComponents.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MotionAnimator/MotionAnimator.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MotionInterchange/MotionInterchange.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/abseil/absl.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/gRPC-C++/grpcpp.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/gRPC-Core/grpc.framework"
